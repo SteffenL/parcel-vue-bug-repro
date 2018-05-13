7b55170d63e42ed64d7a2b50211519e422518337
Steffen André Langnes <steffenl.sw@gmail.com>
2018-05-13 21:09:12 +0200

    Add code to test problem with multiple Vue components

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,18 @@
 <template>
-<div id="app"></div>
+<div id="app">
+    <ComponentA />
+    <ComponentB />
+</div>
 </template>
 
 <script>
-// Fix #1: change name of the imported file, e.g. to Hello.js
-import Auth from './Auth';
+import ComponentA from './ComponentA.vue';
+import ComponentB from './ComponentB.vue';
 
 export default {
-    // Fix #2: remove data()
-    data() {
-        return {};
-    },
-    mounted() {
-        // Fix #3: change name of isAuthenticated(), e.g. to asdf()
-        return Auth.isAuthenticated().then(() => {
-            this.setStage();
-            // Fix #4: uncomment the following line
-            //console.log(1);
-        });
-    },
-    methods: {
-        // Fix #5: change name of setStage(), e.g. to qwer()
-        setStage() {}
+    components: {
+        ComponentA,
+        ComponentB
     }
 };
 </script>
